3f94f60895f091c70f2e9f5460dca0c36529ed45
Gautam Kumar <128383396+GautamGitHub2@users.noreply.github.com>
2023-11-29 18:50:45 +0530

    Add files via upload

---
+++ ExplicitWaitExample.java
@@ -0,0 +1,32 @@
+package SeleniumWithJavaProject;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class ExplicitWaitExample {
+    public static void main(String[] args) {
+        WebDriverManager.safaridriver().setup();
+        WebDriver driver=new SafariDriver();
+
+        driver.get("https://www.google.com/");
+        driver.manage().window().maximize();
+
+        //Explicit wait
+        WebDriverWait mywait=new WebDriverWait(driver,Duration.ofSeconds(5));
+
+        driver.findElement(By.name("q")).sendKeys("Selenium");
+        driver.findElement(By.name("q")).sendKeys(Keys.RETURN);
+
+        WebElement element= mywait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h3[text()='Selenium WebDriver']")));
+        element.click();
+        
+    }
+    }
